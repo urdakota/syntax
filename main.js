eae49da2d16ef9475f12e26eb7a922ddadb3c855
Dakota <95160810+urdakota@users.noreply.github.com>
2022-02-05 01:59:40 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let URL=window.location.hostname;
 let support = ['blooket','edpuzzle','kahoot','gimkit','quizizz'];
 support.forEach(function(string,index){
   if(URL.toLowerCase().includes(string)){
-    fetch(`('https://urdakota.github.io/syntax/${string}/index.js`).then((res) => res.text().then((t) => eval(t)));
+    fetch(`https://urdakota.github.io/syntax/${string}/index.js`).then((res) => res.text().then((t) => eval(t)));
   }
 
   }
