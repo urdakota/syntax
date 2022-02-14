ac9b58b0e919d57dbdcc0b27a155657ede1818fe
Dakota <95160810+urdakota@users.noreply.github.com>
2022-02-14 12:58:04 -0500

    Update ui.js

--- dependancies/ui.js
+++ dependancies/ui.js
@@ -1,79 +1,71 @@
 console.log("-> Writing it right now");
-var bruh = [];
+window.syntaxui = [];
 // inserting
  var doc = document;
  var bod = doc.body;
 
-window.addEventListener('load', function () {
   var style = document.createElement('style');
   style.type = 'text/css';
   style.innerHTML = `
-   * { font-family: Arial; box-sizing: border-box; -webkit-touch-callout: none; 
-    -webkit-user-select: none; 
-     -khtml-user-select: none; 
-       -moz-user-select: none; 
-        -ms-user-select: none; 
-            user-select: none; 
-   } 
-  .holder {
-   position: fixed;
-   border: 2px solid #505050;
-   overflow: hidden;
-   background-color: #1E1E1E;
-   height: 300px;
-   transition: 0.4s;
-   border-radius: 12px;
-   width: 500px;
-   animation: fadeEffect 0.4s
-  }
-  .list {
-   float: left;
-   border: 2px solid #505050;
-   overflow: hidden;
-   background-color: #1E1E1E;
-   transition: 0.4s;
-   border-radius: 12px;
-   margin: 0px 0px 0px -12px;
-   width: 115px;
-   height: 300px;
- }
-    .list button {
-      display: block;
-      background-color: #1E1E1E;
-      color: grey;
-      padding: 4px 16px;
-      width: 100%;
-      border: 0px solid #2d2d2d;
-      border-radius: 0px;
-      outline: none;
-      text-align: center;
-      cursor: pointer;
-      transition: 0.3s;
-    }
-    .list button:hover {
-      color: #ddd;
-      background-color: #2e2e2e;
-    }
- .list button.active {
-      color: #ff9100;
-      background-color: #474747;
- }
-    .tabcontent {
-      float: right;
-      padding: 0px 6px;
-      width: 393px;
-      margin: -16px 0px 0px -12px;
-      display: none;
-      border-left: none;
-      height: 300px;
+   * { font-family: Arial; box-sizing: border-box; -webkit-touch-callout: none; user-select: none  } 
+   .holder {
+      position: fixed;
+      border: 2px solid #505050;
       overflow: hidden;
+      background-color: #1E1E1E;
+      height: 300px;
+      transition: height 1s, width 1s;
+      border-radius: 12px;
+      width: 500px;
       animation: fadeEffect 0.4s
-  }
-
-  @keyframes fadeEffect {
-    from {opacity: 0;}
-    to {opacity: 1;}
-  }`
+   }
+   .list {
+      float: left;
+      border: 2px solid #505050;
+      overflow: hidden;
+      background-color: #1E1E1E;
+      transition: 0.4s;
+      border-radius: 12px;
+      margin: 0px 0px 0px -12px;
+      width: 115px;
+      height: 300px;
+   }
+   .list button {
+     display: block;
+     background-color: #1E1E1E;
+     color: grey;
+     padding: 4px 16px;
+     width: 100%;
+     border: 0px solid #2d2d2d;
+     border-radius: 0px;
+     outline: none;
+     text-align: center;
+     cursor: pointer;
+     transition: 0.3s;
+   }
+   .list button:hover {
+     color: #ddd;
+     background-color: #2e2e2e;
+   } 
+   .list button.active {
+     color: #ff9100;
+     background-color: #474747;
+   }
+   .tabcontent {
+     float: right;
+     padding: 0px 6px;
+     width: 393px;
+     margin: -16px 0px 0px -12px;
+     display: none;
+     border-left: none;
+     height: 300px;
+     overflow: hidden;
+     animation: fadeEffect 0.4s
+   }
+   @keyframes fadeEffect {
+     from {opacity: 0;}
+     to {opacity: 1;}
+   }`
 document.getElementsByTagName('head')[0].appendChild(style);
  // UI
   var holder = doc.createElement("div");
@@ -99,8 +91,18 @@ document.getElementsByTagName('head')[0].appendChild(style);
     holder.appendChild(mini);
     mini.setAttributeNode(createatt("id","minimize"));
     mini.setAttributeNode(createatt("style","color:#d7d7d7; margin: -24px 28px 0px 0px; float: right"));
-    mini.setAttributeNode(createatt("onclick","minimize();"));
     mini.innerText = "P ";
+    mini.onclick = function(){
+      if (holder.style.height === "30px") {
+        holder.style.height = "300px";
+        holder.style.width = "500px";
+        titlespantwo.innerText = "─";
+      } else {
+        holder.style.height = "30px";
+        holder.style.width = "156px";
+        titlespantwo.innerText = "+";
+      }
+    }
     var titlespantwo = doc.createElement("span");
      titlespantwo.innerText = "─";
      mini.appendChild(titlespantwo);
@@ -116,17 +118,6 @@ document.getElementsByTagName('head')[0].appendChild(style);
    at.value=val;
    return at;
   }
-  function minimize() {
-      if (document.getElementById("syntax").style.height === "32px") {
-        document.getElementById("syntax").style.height = "300px";
-        document.getElementById("syntax").style.width = "500px";
-        document.getElementById("minimize").innerHTML = "─";
-      } else {
-        document.getElementById("syntax").style.height = "32px";
-        document.getElementById("syntax").style.width = "156px";
-        document.getElementById("minimize").innerHTML = "+";
-      }
-  }
   document.addEventListener("keydown", function() {
       var k = event.keyCode || event.which;
       if (k === 80) {
@@ -137,69 +128,27 @@ document.getElementsByTagName('head')[0].appendChild(style);
       	}
       }
   });
-  var active = false;
-    var currentX;
-    var currentY;
-    var initialX;
-    var initialY;
-    var xOffset = 0;
-    var yOffset = 0;
-
-    title.addEventListener("touchstart", dragStart, false);
-    title.addEventListener("touchend", dragEnd, false);
-    title.addEventListener("touchmove", drag, false);
-
-    title.addEventListener("mousedown", dragStart, false);
-    title.addEventListener("mouseup", dragEnd, false);
-    title.addEventListener("mousemove", drag, false);
-
-    function dragStart(e) {
-      if (e.type === "touchstart") {
-        initialX = e.touches[0].clientX - xOffset;
-        initialY = e.touches[0].clientY - yOffset;
-      } else {
-        initialX = e.clientX - xOffset;
-        initialY = e.clientY - yOffset;
-      }
-
-      if (e.target === title) {
-        active = true;
-      }
-    }
-
-    function dragEnd(e) {
-      initialX = currentX;
-      initialY = currentY;
-
-      active = false;
-    }
-
-    function drag(e) {
-      if (active) {
-      
-        e.preventDefault();
-      
-        if (e.type === "touchmove") {
-          currentX = e.touches[0].clientX - initialX;
-          currentY = e.touches[0].clientY - initialY;
-        } else {
-          currentX = e.clientX - initialX;
-          currentY = e.clientY - initialY;
-        }
-
-        xOffset = currentX;
-        yOffset = currentY;
-
-        setTranslate(currentX, currentY, holder);
-      }
-    }
-
-    function setTranslate(xPos, yPos, el) {
-      el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
-    }
+  
+  holder.addEventListener('mousedown', function(e) {
+    var offsetX = e.clientX - parseInt(window.getComputedStyle(this).left);
+    var offsetY = e.clientY - parseInt(window.getComputedStyle(this).top);
     
-  // bruh
-   bruh.tab = function(name){
+    function mouseMoveHandler(e) {
+      holder.style.top = (e.clientY - offsetY) + 'px';
+      holder.style.left = (e.clientX - offsetX) + 'px';
+    }
+
+    function reset() {
+      window.removeEventListener('mousemove', mouseMoveHandler);
+      window.removeEventListener('mouseup', reset);
+    }
+
+    window.addEventListener('mousemove', mouseMoveHandler);
+    window.addEventListener('mouseup', reset);
+  });
+    
+  // syntaxui
+   syntaxui.tab = function(name){
     let tabtbl = [];
     let btn = doc.createElement("button");
      list.appendChild(btn);
@@ -232,11 +181,17 @@ document.getElementsByTagName('head')[0].appendChild(style);
         tab.appendChild(tabholder);
         tabholder.setAttributeNode(createatt("id",name+"holder"));
         tabholder.setAttributeNode(createatt("class","tabcontent"));
-
-      
+    tabtbl.button = function(name,callback){
+      let btntbl = [];
+      return btntbl;
+    }
+    tabtbl.toggle = function(name,callback){
+      let tgltbl = [];
+      return tgltbl;
+    }
+    tabtbl.button = function(name,callback){
+      let btntbl = [];
+      return btntbl;
+    }
     return tabtbl;
    };
-  
- let HomeTab = bruh.tab("Home");
- let creamTab = bruh.tab("Combat");
-});
